77639de956f4217921821fc1ec2473ba0acb35f9
Ayden <ayden12213223platypu@gmail.com>
2025-08-03 15:59:12 -0400

    Add files via upload

---
+++ default.lua
@@ -0,0 +1,104 @@
+return {
+    misc = {
+        challenge_names = {
+            c_ultcha_demo_no_editions = "Challenger Examples (Demo): Complete edition ban",
+            c_ultcha_demo_individual_edition_bans = "Challenger Examples (Demo): Individual edition ban",
+            c_ultcha_demo_money_total_scaling = "Challenger Examples (Demo): Money scaling",
+            c_ultcha_demo_disable_hand_containing = "Challenger Examples (Demo): Hand disabling",
+            c_ultcha_demo_whitelist_hand = "Challenger Examples (Demo): Hand whitelisting",
+            c_ultcha_demo_blackjack = "Challenger Examples (Demo): Blackjack",
+            c_ultcha_demo_jokers_per_dollar = "Challenger Examples (Demo): +Joker slots per $",
+            c_ultcha_demo_overflow_debuff = "Challenger Examples (Demo): Overflow debuff",
+            c_ultcha_demo_overflow_debuff_one = "Challenger Examples (Demo): Debuff per overflow",
+            c_ultcha_demo_rental_hell = "Challenger Examples (Demo): Rental rules",
+            c_ultcha_demo_perishable_hell = "Challenger Examples (Demo): Perishable rules",
+            c_ultcha_demo_no_vouchers = "Challenger Examples (Demo): No Vouchers",
+            c_ultcha_demo_shop_hell = "Challenger Examples (Demo): Shop rules",
+            c_ultcha_demo_forced_joker_pool = "Challenger Examples (Demo): Forced Joker pool",
+            c_ultcha_demo_disabling = "Challenger Examples (Demo): Disabling suits and ranks",
+            c_ultcha_demo_extra_hand_money_scaling = "Challenger Examples (Demo): Money per spare hand",
+            c_ultcha_luck_have_it = "Challenger Plus: As Luck Would Have It",
+            c_ultcha_brick_n_wall = "Challenger Plus: Another Brick In The Wall",
+            c_ultcha_flint_steel = "Challenger Plus: Flint And Steel",
+            c_ultcha_dropping = "Challenger Plus: Constant Dropping",
+            c_ultcha_kind_chall = "Challenger Plus: Kind Enough",
+            c_ultcha_wicked = "Challenger Plus: Ain't No Rest For The Wicked",
+            c_ultcha_papermaker = "Challenger Plus: Paper Maker",
+            c_ultcha_wild_thing = "Challenger Plus: Wild Thing",
+            c_ultcha_junky = "Challenger Plus: Magic Junkyard",
+            c_ultcha_storm = "Challenger Plus: Storm In A Seltzer",
+            c_ultcha_circus = "Challenger Plus: Complete Circus",
+            c_ultcha_broken_gd = "Challenger Plus: Broken Game Design",
+            c_ultcha_crushed_hand = "Challenger Plus: Hand Crushed By A Mallet",
+            c_ultcha_fortknox = "Challenger Plus: Fort Knox",
+        },
+        v_text = {
+            ch_c_ultcha_mult_reduce = {
+                "Base {C:red}Mult{} is 1"
+            },
+            ch_c_ultcha_hand_destroying = {
+                "At the end of round remaining cards in hand become destoroyed"
+            },
+            ch_c_ultcha_of_a_kind = {
+                "Only {C:attention}Three of a Kind{}, {C:attention}Four of a Kind{} and {C:attention}Five of a Kind{} hands are allowed"
+            },
+            ch_c_ultcha_no_shop_planets = {
+                "Planet cards no longer appear in the {C:attention}shop"
+            },
+            ch_c_ultcha_no_shop_tarots = {
+                "Tarot cards no longer appear in the {C:attention}shop"
+            },
+            ch_c_ultcha_shop_spectrals = {
+                "Spectral cards appear in the {C:attention}shop"
+            },
+            ch_c_ultcha_no_shops = {
+                "{C:attention}Shops{} are skipped"
+            },
+            ch_c_ultcha_selzer_maker = {
+                "At the start of each Ante create a {C:dark_edition}Negative{} {C:attention}Selzer{}",
+            },
+            ch_c_ultcha_storm_ante = {
+                "Ante scales higher"
+            },
+            ch_c_ultcha_ten_jokers = {
+                "You need to have at least {C:attention}8{} Jokers at the end of Ante 8"
+            },
+            ch_c_ultcha_queen_check = {
+                "You need to have five Queens in your full deck at the end of Ante 8"
+            },
+            ch_c_ultcha_hands_n_discards = {
+                "Limited amount of {C:blue}hands{} and {C:red}discards{}"
+            },
+            ch_c_ultcha_random_hand_size = {
+                "Randomize hand size at the start of round"
+            },
+            ch_c_ultcha_random_planets = {
+                "Planet cards upgrades random {C:attention}poker hands"
+            },
+            ch_c_ultcha_big_finisher = {
+                "Finisher boss scaling {C:attention}x10"
+            },
+            ch_c_ultcha_crap_factory = {
+                "Only the worst Jokers"
+            },
+            ch_c_ultcha_plasma = {
+                "Balance {C:blue}Chips{} and {C:red}Mult{} when calculating score for played hand"
+            },
+            ch_c_ultcha_more_bosses = {
+                "Each Blind is a {C:attention}Boss Blind"
+            },
+            ch_c_ultcha_no_skips = {
+                "Skipping Blind isn't available"
+            },
+            ch_c_ultcha_consolation_prize = {
+                "Earn {C:money}$3{} at end of round"
+            },
+            ch_c_ultcha_hide_blind = {
+                "{C:attention}Boss Blinds{} are hidden"
+            },
+            ch_c_ultcha_higher_ante = {
+                "Winning Ante is {C:attention}Ante 12"
+            }
+        },
+    }
+}
